1feeffc307b7484a7c967fb2b5cb4eafdb18a8e0
KminSpencer <kminspencer@gmail.com>
2016-12-07 19:01:50 -0800

    First Commit

---
+++ Assets/Scr_Health.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Scr_Health : MonoBehaviour {
+
+    public float m_MaxHealth;
+    private float m_CurrentHealth;
+
+    private Rigidbody m_Body;
+
+    private void Start()
+    {
+        m_Body = GetComponent<Rigidbody>();
+        m_CurrentHealth = m_MaxHealth;
+        UpdateHealthbar();
+    }
+    
+    private void UpdateHealthbar()
+    {
+        float ratio = m_CurrentHealth / m_MaxHealth;
+    }
+    private void TakeDamage(float damage, Vector2 direction)
+    {
+        m_CurrentHealth -= damage;
+
+        if(m_CurrentHealth < 0)
+        {
+            m_CurrentHealth = 0;
+        }
+        UpdateHealthbar();
+
+        m_Body.velocity =(new Vector3(direction.x, direction.y,0f));
+    }
+    private void HealDamage(float heal)
+    {
+        m_CurrentHealth += heal;
+
+        if(m_CurrentHealth > m_MaxHealth)
+        {
+            m_CurrentHealth = m_MaxHealth;
+        }
+        UpdateHealthbar();
+    }
+
+}
